e90fd2622c86fe64f81c72d47ce33863a95b1d07
Jimmy Downer <jamesdanieldowner@gmail.com>
2024-12-31 16:15:35 -0700

    Basic blue window

--- src/main.cxx
+++ src/main.cxx
@@ -1,9 +1,83 @@
+#include <cstdlib>
+#include <iostream>
+
 #define GLAD_GL_IMPLEMENTATION
 #include <glad/gl.h>
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
+#ifdef _DEBUG
+#define DEBUG
+#endif
+
+#ifdef DEBUG
+#define LOG(x) do { std::cout << x; } while (false);
+#define LOG_ERROR(x) do { std::cerr << x; } while (false);
+void errorCallbackGLFW(int /*error*/, const char* description) {
+  LOG_ERROR("GLFW error: " << description << '\n');
+}
+void debugMessageCallbackGL(
+  GLenum /*source*/,
+  GLenum /*type*/,
+  GLuint /*id*/,
+  GLenum /*severity*/,
+  GLsizei /*length*/,
+  const GLchar * message,
+  const void* /*userParam*/
+) {
+  LOG_ERROR("GL error: " << message << '\n');
+}
+#else
+#define LOG(x)
+#define LOG_ERROR(x)
+#endif
+
 int main(int, char**) {
-  glfwInit();
+  if (!glfwInit()) {
+    LOG_ERROR("Failed to initialize GLFW\n");
+    std::exit(EXIT_FAILURE);
+  }
+#ifdef DEBUG
+  glfwSetErrorCallback(errorCallbackGLFW);
+#endif
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_DECORATED, true);
+#ifdef DEBUG
+  glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);
+#endif
+#ifdef __APPLE__
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, true);
+#endif
+  GLFWwindow* window{glfwCreateWindow(640, 480, "Hello", nullptr, nullptr)};
+  if (window == nullptr) {
+    LOG_ERROR("Failed to create GLFW window\n");
+    glfwTerminate();
+    std::exit(EXIT_FAILURE);
+  }
+  glfwMakeContextCurrent(window);
   gladLoadGL(glfwGetProcAddress);
+#ifdef DEBUG
+  if (GLAD_GL_ARB_debug_output) {
+    LOG("GL extension GL_ARB_debug_output available\n");
+    glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS_ARB);
+    glDebugMessageCallbackARB(debugMessageCallbackGL, nullptr /*userParam*/);
+  }
+  else {
+    LOG("GL extension GL_ARB_debug_output unavailable\n");
+  }
+#endif
+  LOG("Running...\n");
+  while (!glfwWindowShouldClose(window)) {
+    int width{};
+    int height{};
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+    glClearColor(0., .5, 1., 1.);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+  LOG("Goodbye.\n");
 }
