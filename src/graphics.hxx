d2b2454c2971182c7b436efa01914689baefcad7
Jimmy Downer <jamesdanieldowner@gmail.com>
2025-01-17 16:12:58 -0700

    Default shaders in separate files

--- src/graphics.hxx
+++ src/graphics.hxx
@@ -12,50 +12,13 @@
 
 #include "debug.hxx"
 
-constexpr const char* defaultVertexSource{R"(#version 110
-#define positionOut gl_Position
-#if __VERSION__ <= 130
-attribute vec3 position;
-#else
-in vec3 position;
-#endif
+constexpr const char* defaultVertexSource{
+#include "default.vert"
+};
 
-uniform float time;
-uniform ivec2 resolution;
-
-void main(void) {
-  positionOut = vec4(position, 1.);
-}
-)"};
-
-constexpr const char* defaultFragmentSource{R"(#version 110
-#define fragCoordIn gl_FragCoord
-#if __VERSION__ <= 130
-#define fragColorOut gl_FragColor
-#else
-out vec4 fragColorOut;
-#endif
-
-uniform float time;
-uniform ivec2 resolution;
-
-vec4 hsv2rgba(in vec3 hsv) {
-  float h = hsv.x;
-  float s = hsv.y;
-  float v = hsv.z;
-  vec3 k = vec3(1., 2./3., 1./3.);
-  vec3 p = clamp(abs(6.*fract(h - k) - 3.) - 1., 0., 1.);
-  return vec4(v * mix(k.xxx, p, s), 1.);
-}
-
-void main(void) {
-  vec2 center = vec2(resolution)*.5;
-  float scale = min(float(resolution.x), float(resolution.y));
-  vec2 newCenter = (fragCoordIn.xy - center)/scale;
-  vec3 hsv = vec3(newCenter.y - newCenter.x + time/10., 1., 1.);
-  fragColorOut = hsv2rgba(hsv);
-}
-)"};
+constexpr const char* defaultFragmentSource{
+#include "default.frag"
+};
 
 template<GLsizei V, GLsizei I>
 class Model {
